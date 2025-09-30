2dc5192d7bdfa822327e4fa5534cbc7b70e5331e
benjaminwfsdf <benjagonzalito08@gmail.com>
2025-09-30 16:27:27 -0300

    Actualizar service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 /* service-worker.js */
-const CACHE_VER = 'arcade-v12';
+const CACHE_VER = 'arcade-v14';
 const STATIC_CACHE = CACHE_VER + '-static';
 const RUNTIME_CACHE = CACHE_VER + '-runtime';
 
